a4210310f021944ef1928ebc0641f25f7228b296
veezyjay <viktorbasi545@gmail.com>
2020-04-08 22:16:26 +0100

    Carry out cleanup of unused imports and logs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Button } from "antd";
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import NotFound from "./components/pages/NotFound";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import Dashboard from "./components/pages/Dashboard";
+import SeasonState from "./context/season/SeasonState";
+import CustomerState from "./context/customer/CustomerState";
+import RepaymentState from "./context/repayment/RepaymentState";
+import "./App.css";
 
 function App() {
   return (
-    <Button type="primary">
-      My App
-    </Button>
+    <SeasonState>
+      <CustomerState>
+        <RepaymentState>
+          <Router>
+            <Navbar />
+            <Switch>
+              <Route exact path="/dashboard" component={Dashboard}></Route>
+              <Route exact path="/about" component={About}></Route>
+              <Route exact path="/" component={Home}></Route>
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </RepaymentState>
+      </CustomerState>
+    </SeasonState>
   );
 }
 
