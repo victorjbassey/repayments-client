ce1066ed569eb94d9ae3cb65d314e460cc4aa41f
veezyjay <viktorbasi545@gmail.com>
2020-04-08 10:18:43 +0100

    Add about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About this App</h1>
+      <p>App to make loan repayments</p>
+      <p>Version 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
