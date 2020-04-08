a024ba8e592d6d470990a65c74dfa9c8de98616a
veezyjay <viktorbasi545@gmail.com>
2020-04-08 22:03:26 +0100

    Update home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <main className="home">
-      <div class="home__textbox">
-        <h1 class="home__heading">
-          <span class="home__heading--main">the portal</span>
-          <span class="home__heading--sub">for all repayments</span>
+      <div className="home__textbox">
+        <h1 className="home__heading">
+          <span className="home__heading--main">the portal</span>
+          <span className="home__heading--sub">for all repayments</span>
         </h1>
         <Link to="/dashboard">
           <button className="home__btn btn btn--white" href="#">
