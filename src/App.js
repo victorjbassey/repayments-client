a296279ec250407a01c6ab16279aab800ac9cb97
veezyjay <viktorbasi545@gmail.com>
2020-04-08 09:40:10 +0100

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from "antd";
+import './App.css';
+
+function App() {
+  return (
+    <Button type="primary">
+      My App
+    </Button>
+  );
+}
+
+export default App;
